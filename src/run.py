a3d20e7424f2f1e06ec6493d0404846c998ecb29
Jack Lynch <jacklynchtds@gmail.com>
2018-04-28 18:56:58 -0400

    Update run.py

--- src/run.py
+++ src/run.py
@@ -45,65 +45,65 @@ if __name__ == '__main__':
     image = TfPoseEstimator.draw_humans(image, humans, imgcopy=False)
     cv2.imshow('tf-pose-estimation result', image)
     # cv2.waitKey()
-"""
-    import matplotlib.pyplot as plt
 
-    fig = plt.figure()
-    a = fig.add_subplot(2, 2, 1)
-    a.set_title('Result')
-    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    #import matplotlib.pyplot as plt
 
-    bgimg = cv2.cvtColor(image.astype(np.uint8), cv2.COLOR_BGR2RGB)
-    bgimg = cv2.resize(bgimg, (e.heatMat.shape[1], e.heatMat.shape[0]), interpolation=cv2.INTER_AREA)
+    #fig = plt.figure()
+    #a = fig.add_subplot(2, 2, 1)
+    #a.set_title('Result')
+    #plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+
+    #bgimg = cv2.cvtColor(image.astype(np.uint8), cv2.COLOR_BGR2RGB)
+    #bgimg = cv2.resize(bgimg, (e.heatMat.shape[1], e.heatMat.shape[0]), interpolation=cv2.INTER_AREA)
 
     # show network output
-    a = fig.add_subplot(2, 2, 2)
-    plt.imshow(bgimg, alpha=0.5)
-    tmp = np.amax(e.heatMat[:, :, :-1], axis=2)
-    plt.imshow(tmp, cmap=plt.cm.gray, alpha=0.5)
-    plt.colorbar()
+    #a = fig.add_subplot(2, 2, 2)
+    #plt.imshow(bgimg, alpha=0.5)
+    #tmp = np.amax(e.heatMat[:, :, :-1], axis=2)
+    #plt.imshow(tmp, cmap=plt.cm.gray, alpha=0.5)
+    #plt.colorbar()
 
-    tmp2 = e.pafMat.transpose((2, 0, 1))
-    tmp2_odd = np.amax(np.absolute(tmp2[::2, :, :]), axis=0)
-    tmp2_even = np.amax(np.absolute(tmp2[1::2, :, :]), axis=0)
+    #tmp2 = e.pafMat.transpose((2, 0, 1))
+    #tmp2_odd = np.amax(np.absolute(tmp2[::2, :, :]), axis=0)
+    #tmp2_even = np.amax(np.absolute(tmp2[1::2, :, :]), axis=0)
 
-    a = fig.add_subplot(2, 2, 3)
-    a.set_title('Vectormap-x')
+    #a = fig.add_subplot(2, 2, 3)
+    #a.set_title('Vectormap-x')
     # plt.imshow(CocoPose.get_bgimg(inp, target_size=(vectmap.shape[1], vectmap.shape[0])), alpha=0.5)
-    plt.imshow(tmp2_odd, cmap=plt.cm.gray, alpha=0.5)
-    plt.colorbar()
+    #plt.imshow(tmp2_odd, cmap=plt.cm.gray, alpha=0.5)
+    #plt.colorbar()
 
-    a = fig.add_subplot(2, 2, 4)
-    a.set_title('Vectormap-y')
+    #a = fig.add_subplot(2, 2, 4)
+    #a.set_title('Vectormap-y')
     # plt.imshow(CocoPose.get_bgimg(inp, target_size=(vectmap.shape[1], vectmap.shape[0])), alpha=0.5)
-    plt.imshow(tmp2_even, cmap=plt.cm.gray, alpha=0.5)
-    plt.colorbar()
-    plt.show()
+    #plt.imshow(tmp2_even, cmap=plt.cm.gray, alpha=0.5)
+    #plt.colorbar()
+    #plt.show()
 
-    import sys
-    sys.exit(0)
+    #import sys
+    #sys.exit(0)
 
-    logger.info('3d lifting initialization.')
-    poseLifting = Prob3dPose('./src/lifting/models/prob_model_params.mat')
+    #logger.info('3d lifting initialization.')
+    #poseLifting = Prob3dPose('./src/lifting/models/prob_model_params.mat')
 
-    image_h, image_w = image.shape[:2]
-    standard_w = 640
-    standard_h = 480
+    #image_h, image_w = image.shape[:2]
+    #standard_w = 640
+    #standard_h = 480
 
-    pose_2d_mpiis = []
-    visibilities = []
-    for human in humans:
-        pose_2d_mpii, visibility = common.MPIIPart.from_coco(human)
-        pose_2d_mpiis.append([(int(x * standard_w + 0.5), int(y * standard_h + 0.5)) for x, y in pose_2d_mpii])
-        visibilities.append(visibility)
+    #pose_2d_mpiis = []
+    #visibilities = []
+    #for human in humans:
+    #    pose_2d_mpii, visibility = common.MPIIPart.from_coco(human)
+    #    pose_2d_mpiis.append([(int(x * standard_w + 0.5), int(y * standard_h + 0.5)) for x, y in pose_2d_mpii])
+    #    visibilities.append(visibility)
 
-    pose_2d_mpiis = np.array(pose_2d_mpiis)
-    visibilities = np.array(visibilities)
-    transformed_pose2d, weights = poseLifting.transform_joints(pose_2d_mpiis, visibilities)
-    pose_3d = poseLifting.compute_3d(transformed_pose2d, weights)
+    #pose_2d_mpiis = np.array(pose_2d_mpiis)
+    #visibilities = np.array(visibilities)
+    #transformed_pose2d, weights = poseLifting.transform_joints(pose_2d_mpiis, visibilities)
+    #pose_3d = poseLifting.compute_3d(transformed_pose2d, weights)
 
-    for i, single_3d in enumerate(pose_3d):
-        plot_pose(single_3d)
-    plt.show()
-"""
+    #for i, single_3d in enumerate(pose_3d):
+    #    plot_pose(single_3d)
+    #plt.show()
+
     pass
